a45194fdc7a7e54ac6745e6f68b49ce4a8a99f79
bccantwell <bcantwell@mills.edu>
2019-02-11 14:21:19 -0800

    Created onPause and OnResume methods.

--- app/src/main/java/com/example/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/example/stopwatch/StopwatchActivity.java
@@ -1,13 +1,91 @@
 package com.example.stopwatch;
 
 import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.view.View;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.os.Handler;
+import android.util.Log;
+import java.util.Locale;
 
-public class StopwatchActivity extends AppCompatActivity {
+public class StopwatchActivity extends Activity {
+    private int seconds = 0;
+    private boolean running;
+    private boolean wasRunning;
+    private static final int SECS_PER_MIN = 60;
+    private static final int MINS_PER_HOUR = 60;
+    private static final int SECS_PER_HOUR = SECS_PER_MIN * MINS_PER_HOUR;
+    private static final int DELAY_MILLIS = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        if (savedInstanceState != null){
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
+        }
+        runTimer();
+        Log.d("StopwatchActivity", "In onCreate");
     }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+
+    public void onClickStart(View view){
+        running = true;
+    }
+
+    public void onClickStop(View view){
+        running = false;
+    }
+
+    public void onClickReset(View view){
+        running = false;
+        seconds = 0;
+    }
+
+    private void runTimer() {
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / SECS_PER_HOUR;
+                int minutes = (seconds % SECS_PER_HOUR) / MINS_PER_HOUR;
+                int secs = seconds % SECS_PER_MIN;
+                String time = String.format(Locale.getDefault(),
+                        "%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    Log.d("Time between increments", "Incremented seconds: " + seconds);
+                    seconds++;
+
+                }
+                handler.postDelayed(this, DELAY_MILLIS);
+            }
+        });
+    }
+
 }
